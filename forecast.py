11b5681fd132def576597f8942ab785724a220e2
Eric Hare <ericrhare@gmail.com>
2022-03-28 16:37:45 -0700

    Create forecast.py

---
+++ forecast.py
@@ -0,0 +1,10 @@
+#from neuralprophet import NeuralProphet
+import pandas as pd
+
+def forecast(df):
+    return 10
+    # m = NeuralProphet(changepoints=["2020-03-31"])
+    # m.add_country_holidays("US")
+    # future = m.make_future_dataframe(df, periods=365)
+    # forecast =  m.predict(future)
+    # return forecast
